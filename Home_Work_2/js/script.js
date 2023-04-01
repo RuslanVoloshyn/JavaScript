413385cc6ae54f40d929a2d27054a84a60796b44
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2023-04-01 16:07:13 +0200

    number

---
+++ Home_Work_2/js/script.js
@@ -0,0 +1,18 @@
+//Task one
+const firstName = prompt("What is your name");
+const country = prompt("Where are you from");
+const hobby = prompt("Which you have a hobby");
+
+alert(
+	`Hello my Name Is ${firstName} \n I'm from ${country} \n I like to do ${hobby}`
+);
+// Task Two
+const num = "12345";
+
+console.log(
+	num.charAt(4),
+	num.charAt(1),
+	num.charAt(0),
+	num.charAt(3),
+	num.charAt(2)
+);
