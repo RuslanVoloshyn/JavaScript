b21fbcffd6cb97802e6034cc065c3f618387ff58
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2023-04-03 16:08:26 +0200

    work_5

---
+++ Home_Work_5/js/script.js
@@ -0,0 +1,7 @@
+//We get from the user via PROmpt three numbers
+const firstNum = Number(prompt("Specify the first number"));
+const twoNum = Number(prompt("Specify the second number"));
+const thirdNum = Number(prompt("Specify the third number"));
+const participants = 3;
+//show through Alert arithmetic of these numbers
+alert(`${(firstNum + twoNum + thirdNum) / participants}`);
