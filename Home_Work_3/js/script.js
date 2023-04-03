2d69a180a38eda30631894c92fbed6b3a9fec543
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2023-04-03 14:46:41 +0200

    work2

---
+++ Home_Work_3/js/script.js
@@ -0,0 +1,9 @@
+const frstNum = Number(prompt("First nuber"));
+const scnNum = Number(prompt("Second nuber"));
+alert(
+	`${frstNum} + ${scnNum} = ${frstNum + scnNum}\n${frstNum} - ${scnNum} = ${
+		frstNum - scnNum
+	}\n${frstNum} * ${scnNum} = ${frstNum * scnNum}\n${frstNum} / ${scnNum} = ${
+		frstNum / scnNum
+	}`
+);
