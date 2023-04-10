3b098c726a789a11dd7ef5c952566143f50e6f93
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2023-04-10 12:17:49 +0200

    calc

---
+++ Home_Work_6/js/script.js
@@ -0,0 +1,24 @@
+//We ask the user of the task and conditionscons
+const chOperand = prompt(
+	'Print the name of operand you want to choose: "Add","Sub","Mult" or "Div"'
+);
+const frstNum = Number(prompt("Print first number"));
+const scndNum = Number(prompt("Print second number"));
+
+switch (chOperand) {
+	case "add":
+		alert(`${frstNum} + ${scndNum} = ${frstNum + scndNum}`);
+		break;
+	case "sub":
+		alert(`${frstNum} - ${scndNum} = ${frstNum - scndNum}`);
+		break;
+	case "mult":
+		alert(`${frstNum} * ${scndNum} = ${frstNum * scndNum}`);
+		break;
+	case "div":
+		alert(`${frstNum} / ${scndNum} = ${frstNum / scndNum}`);
+		break;
+	default:
+		alert("Some Exception");
+		break;
+}
