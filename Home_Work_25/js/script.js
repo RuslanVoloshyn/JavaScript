6e5acc1ac023d917ad9540bcd99f5dc9d41dfeb9
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2023-05-03 11:06:26 +0200

    slider

---
+++ Home_Work_25/js/script.js
@@ -0,0 +1,35 @@
+const imgArr = ["images/1.jpg", "images/2.jpg", "images/3.jpg", "images/4.jpg"];
+
+let imageTarget = 0;
+const prevBtn = document.querySelector(".slider__btn-prev");
+const nextBtn = document.querySelector(".slider__btn-next");
+const elemImage = document.querySelector(".slider__image");
+
+function hiddenBtn() {
+	if (imageTarget === 0) {
+		prevBtn.disabled = true;
+		prevBtn.style.opacity = 0;
+	} else if (imageTarget === imgArr.length - 1) {
+		nextBtn.disabled = true;
+		nextBtn.style.opacity = 0;
+	} else {
+		nextBtn.disabled = false;
+		prevBtn.disabled = false;
+		nextBtn.style.opacity = 1;
+		prevBtn.style.opacity = 1;
+	}
+}
+
+prevBtn.addEventListener("click", () => {
+	imageTarget--;
+	elemImage.setAttribute("src", imgArr[imageTarget]);
+	hiddenBtn();
+});
+
+nextBtn.addEventListener("click", () => {
+	imageTarget++;
+	elemImage.setAttribute("src", imgArr[imageTarget]);
+	hiddenBtn();
+});
+
+hiddenBtn();
